6abee469957e9dc1ee1b4f2a2e264b2d2d4e4842
Davi <davi25090@gmail.com>
2024-01-25 11:57:03 -0300

    :tada: inicial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a img
+  if (html.classList.contains("light")) {
+    //se tiver com light mode adicionar a img
+    img.setAttribute('src', "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Diego Mendes, em um local de Dia")
+  } else {
+    //se tiver sem light mode manter
+    img.setAttribute("src", "./assets/diego01.png")
+    img.setAttribute("alt", "Foto de Diego Mendes, em um local de Noite")
+  }
+
+}
